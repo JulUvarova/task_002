bbe2cb2375939575e97582b542b8ffc769d7a37e
bronwen <uvarovater@yandex.ru>
2024-03-01 20:26:23 +0300

    Q_2 пройдены тесты

--- src/pascal_matrix.c
+++ src/pascal_matrix.c
@@ -4,6 +4,7 @@
 #define NMAX 10
 
 int input(int* n);
+void output(int matrix[NMAX][NMAX], int n);
 double get_factorial(int x);
 int* get_pyramid(int n);
 
@@ -17,35 +18,41 @@ int main(void) {
     int arr[size];
     int count = 0;
     int N = 0;
-    while (count < size) {
+    while (1) {
         for (int j = 0; j <= N; j++) {
             arr[count] = round(get_factorial(N) / round((get_factorial(j) * get_factorial(N - j))));
             count++;
+            if (count == size) break;
         }
         N++;
+        if (count == size) break;
     }
     int matrix[NMAX][NMAX];
     count = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             matrix[i][j] = arr[count];
-            printf("%d", matrix[i][j]);
-            if (j < n - 1) {
-                printf(" ");
-            }
             count++;
         }
-        if (i != n - 1) {
-            printf("\n");
-        }
     }
+    output(matrix, n);
     return 0;
 }
 
 int input(int* n) {
     int exit = 0;
-    if (scanf("%d", n) != 1 || *n < 1 || *n > 10) exit = 1;
+    if (scanf("%d", n) != 1 || *n < 1 || *n > NMAX) exit = 1;
     return exit;
+}
+
+void output(int matrix[NMAX][NMAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d", matrix[i][j]);
+            if (j < n - 1) printf(" ");
+        }
+        if (i != n - 1) printf("\n");
+    }
 }
 
 double get_factorial(int x) {
